89fe506ca203c3d3b3579d94a7ab702b00f62d76
boberstarosta <boberstarosta@gmail.com>
2018-02-03 00:23:45 +0100

    Followed tutorial up to part 10.

--- music/models.py
+++ music/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Album(models.Model):
+    artist = models.CharField(max_length=256)
+    album_title = models.CharField(max_length=512)
+    genre = models.CharField(max_length=128)
+    album_logo = models.CharField(max_length=1024)
+
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=16)
+    song_title = models.CharField(max_length=256)
