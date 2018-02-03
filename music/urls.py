22b33358551b39a60949c5f5787866414cecc62a
boberstarosta <boberstarosta@gmail.com>
2018-02-03 22:30:33 +0100

    Completed part 29 - generic ListView and DetailView

--- music/urls.py
+++ music/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = "music"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:album_id>/', views.detail, name='detail'),
-    path('<int:album_id>/favourite/', views.favourite, name='favourite'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
