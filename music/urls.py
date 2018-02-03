382d61bae8d6e3a0bf7b47bcfce74e3d5cfcc01b
boberstarosta <boberstarosta@gmail.com>
2018-02-03 23:56:10 +0100

    Completed part 32 - UpdateView and DeleteView

--- music/urls.py
+++ music/urls.py
@@ -7,5 +7,7 @@ app_name = "music"
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('album/add/', views.AlbumCreate.as_view(), name='album-add'),
+    path('album/add/', views.AlbumCreate.as_view(), name='album_add'),
+    path('album/<int:pk>/', views.AlbumUpdate.as_view(), name='album_update'),
+    path('album/<int:pk>/delete', views.AlbumDelete.as_view(), name='album_delete'),
 ]
