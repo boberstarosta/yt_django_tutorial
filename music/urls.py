21d79875ae266c18ab8272107c5951317033bd99
boberstarosta <boberstarosta@gmail.com>
2018-02-03 18:39:15 +0100

    Completed part 20 - namespace and 404 shortcut

--- music/urls.py
+++ music/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 
+app_name = "music"
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:album_id>/', views.detail, name='detail'),
